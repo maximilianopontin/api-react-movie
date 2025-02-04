e27489ad7fdb7061a8f0d2e69b7a4b5de9743bc2
maximilianopontin <maxipontin92@gmail.com>
2025-02-04 18:24:52 -0300

    d

--- src/api/getmovies.js
+++ src/api/getmovies.js
@@ -1,19 +1,4 @@
+//const Api = process.env.REACT_APP_API_URL || "http://localhost:3000";  // Usa localhost en desarrollo
 
-
-const Api = import.meta.env.VITE_API_URL || "https://localhost:8080";
-
-export const getMovies = async () => {
-  try {
-    const response = await fetch(`${Api}/movies`);
-    if (!response.ok) {
-      throw new Error(`Error ${response.status}: ${response.statusText}`);
-    }
-    const data = await response.json();
-    console.log("Datos de la API:", data); // Verifica la estructura
-    return Array.isArray(data) ? data : [];
-  } catch (error) {
-    console.error("Error en getMovies:", error);
-    return [];
-  }
-};
-
+const Api = import.meta.env.VITE_API_URL || "https://api-nest-movies.onrender.com"
+export const getMovies = () => fetch(`${Api}/movies`);
