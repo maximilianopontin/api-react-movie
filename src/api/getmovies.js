0a111b05daece320764871b895db7df7486f4a02
maximilianopontin <maxipontin92@gmail.com>
2025-02-04 17:29:18 -0300

    correcion de .env

--- src/api/getmovies.js
+++ src/api/getmovies.js
@@ -1,6 +1,6 @@
 //const Api = process.env.REACT_APP_API_URL || "http://localhost:3000";  // Usa localhost en desarrollo
 
-const Api = import.meta.env.VITE_API_URL || "https://api-nest-movie.onrender.com";
+const Api = import.meta.env.VITE_API_URL || "https://api-nest-movies.onrender.com";
 
 export const getMovies = async () => {
   try {
