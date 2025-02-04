4d8e50e678019e5139f77b6b336d269dc915ad2a
maximilianopontin <maxipontin92@gmail.com>
2025-02-04 18:06:35 -0300

    d

--- src/api/getmovies.js
+++ src/api/getmovies.js
@@ -1,6 +1,6 @@
-//const Api = process.env.REACT_APP_API_URL || "http://localhost:3000";  // Usa localhost en desarrollo
 
-const Api = import.meta.env.VITE_API_URL || "https://api-nest-movies.onrender.com";
+
+const Api = import.meta.env.VITE_API_URL || "https://localhost:8080";
 
 export const getMovies = async () => {
   try {
